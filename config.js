54c99ab8e38ecc86f3bacafafe9cdf3fb8868678
ccty3322 <164542552+ccty3322@users.noreply.github.com>
2026-07-09 09:44:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ window.Config = {
     },
     {
       text: '高配',
-      url: 'https://3ds.ssssss.indevs.in/'
+      url: 'https://griselda-cdlwydcwjv.dcdeploy.cloud/'
     },
     {
       text: 'Uptime',
